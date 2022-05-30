344843803bc0ff61852305a372455c311c661462
twojaytech <65775982+twojaytech@users.noreply.github.com>
2022-05-30 13:53:48 -0400

    initial commit for user router and controller

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,8 +1,18 @@
 const { Pool } = require('pg');
 
-const PG_URI = 'postgres://nopyulan:WKLiI-Qx46dvnkVPu-YRJjbKuBBi9oJp@fanny.db.elephantsql.com/nopyulan'
+// Local Database - Postgres
+const credentials = {
+  user: "postgres",
+  host: "localhost",
+  database: "wanderbud",
+  password: "pass",
+  port: 5432,
+};
+const pool = new Pool(credentials);
 
-const pool = new Pool({connectionString: PG_URI});
+// Remote Database - ElephantSQL
+// const PG_URI = 'postgres://nopyulan:WKLiI-Qx46dvnkVPu-YRJjbKuBBi9oJp@fanny.db.elephantsql.com/nopyulan'
+// const pool = new Pool({connectionString: PG_URI});
 
 module.exports = {
     query: (text, params, callback) => {
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,34 +3,82 @@ const db = require('../../database/dbConnection');
 
 const userController = {};
 
-// Middleware to Add New User
-userController.addUser = (req, res, next) => {
-/* 
-    console.log('Controller AddComic: ', req.body);
-    const {image, title, price} = req.body;
+// Middleware to Add New User (Signup + Login)
+userController.signupUser = (req, res, next) => {
 
-    // error relation table does not exist if not quoted
-    const text ='INSERT INTO "Bookmarks" (image, title, price) VALUES ($1, $2, $3)';
-    const values = [image, title, price];
+    // Log Request Body
+    console.log('-- User Contoller: Add User -- \nreq.body: ', req.body);
+
+    // Check For Any Missing Fields
+    const {firstName, lastName, age, email, password} = req.body;
+    if (!firstName || !lastName || !age || !email || !password) {
+        // res.status(400);
+        // res.json({}); to send back to front end?
+        return next({
+            'log': 'User Controller: Signup User - Missing Required Signup Information', 
+            'message': {err: 'userController.signupUser: ERROR: Missing Required Signup Information'}
+          });
+    }
+
+    // Email Validation
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!re.test(email)) {
+        return next({
+            'log': 'User Controller: Signup User - Incorrect Email', 
+            'message': {err: 'userController.signupUser: ERROR: Incorrect Email'}
+          });
+    }
+
+    // Query - Insert New User Into Database
+    const text ='INSERT INTO "user" ("firstName", "lastName", "age", "email", "password") VALUES ($1, $2, $3, $4, $5) RETURNING "id", "firstName", "lastName", "age", "email";';
+    const values = [firstName, lastName, age, email, password];
 
     db.query(text, values)
         .then(response => {
             console.log('Inserted');
+            res.locals.userData = response.rows[0];
             next();
-        }) */
+        })
+        .catch(err => {
+            // res.render('../../client/components/LoginDisplay.jsx')
+            // res.status(400);
+            return next({
+                'log': 'User Controller: Add User - DB Query Error', 
+                'message': {err: 'userController.addUser: ERROR: check server logs for details'}
+              });
+        })
 }
 
-// Middleware to Get User
-userController.getUser = (req, res, next) => {
+// Middleware to Get User (Login)
+userController.loginUser = (req, res, next) => {
 
-    // console.log('Controller GetComics');
+    console.log('userController.verifyUser for Login');
 
-    // db.query('SELECT * FROM "Bookmarks"')
-    //     .then(response => {
-    //         console.log('Get Comics: ', response.rows);
-    //         res.locals.comics = response.rows;
-    //         next();
-    //     });
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        // res.status(403);
+        return next({
+            'log': 'User Controller: Login User - Missing Email or Password for Login', 
+            'message': {err: 'userController.loginUser: ERROR: Missing Email or Password for Login'}
+          })
+    }
+
+    const text = 'SELECT "id", "firstName", "lastName", "age", "email" FROM "user" WHERE "email"=$1 AND "password"=$2;';
+    const values = [email, password];
+
+    db.query(text, values)
+        .then(response => {
+            console.log('Get User: ', response.rows[0]);
+            res.locals.userData = response.rows[0];
+            next();
+        })
+        .catch(err => {
+            return next({
+                'log': 'Error in userController.getUser DB Query', 
+                'message': {err: 'userController.getUser: ERROR: check server logs for details'}
+              });
+        })
 }
 
 module.exports = userController;
